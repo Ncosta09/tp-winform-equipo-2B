895d157ec8c2eac7db8189eff7b3e44324df9d64
Ncosta09 <ncosta981@gmail.com>
2024-09-04 15:39:29 -0300

    agregado de clase Articulo

---
+++ CRUD/Articulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD
+{
+    internal class Articulo
+    {
+        public int ID { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Precio { get; set; }
+
+    }
+}
